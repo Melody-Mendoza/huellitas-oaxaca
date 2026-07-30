db6da45744e90075cb980babb915c95eb319fa72
Angel-2329 <meganegro29@gmail.com>
2026-07-30 06:53:19 -0600

    Correccion envio de correo

---
+++ backend/src/main/resources/db/migration/V27__actualizar_passwords_cuentas_iniciales.sql
@@ -0,0 +1,35 @@
+UPDATE usuarios
+SET password = '$2a$10$IvcrUnHDeYo10pW5J9xP/evUhEHg.HRLI0Uw2OQOU0fQsYz/Qiqpm'
+WHERE correo = 'adriana.lopez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$OxBt/0JF3osbNXF4flrAqOoqDJg/WgAWUzrOS1xFKWxuCT2mIfUTK'
+WHERE correo = 'carlos.mendez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$5/RfFKn45TT5SSRDbtMJm.70CBigaFiFwQR8n5U3GAz2CnYgq6G6S'
+WHERE correo = 'fernanda.garcia@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$EGrPwhC6knS3leeEBsv8e.r16KnK9BnoUusBaNIhbUZn56cjon.qO'
+WHERE correo = 'daniela.hernandez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$4QPTv4ZRJ0O2tFeQUI5XT.s7AiEHvWr4yS71Ypncr0fkPxev1sB2.'
+WHERE correo = 'jose.ramirez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$erPXs1Au/fFGOVFkwvjo4eStlRFEtdZNwj4hNLe.YhjA/292qC3sa'
+WHERE correo = 'mariana.perez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$OvBsxbqSKPChYiiE9dWj6eIyRZs/obT6ageUHLNfRcYGgJTlgpIq.'
+WHERE correo = 'rosa.vasquez@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$tXZtuwYuG1NUu1N4/3j4QucDX3.n6xcUeDr9vs4kQk/Mk63vLnxk2'
+WHERE correo = 'miguel.cruz@huellitasoaxaca.mx';
+
+UPDATE usuarios
+SET password = '$2a$10$BPi0DAilcvyiWwpkgLUvk.oygRz2teq3hfnWZmi0WX3HeyMG/MH/u'
+WHERE correo = 'elena.sanchez@huellitasoaxaca.mx';
